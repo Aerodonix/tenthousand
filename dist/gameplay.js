98dc9bbeaddb27f1e78a54e3811a23f83008b17e
moep <moep@fedora.fritz.box>
2024-09-12 08:36:28 +0200

    remove node folder and add gitignore

--- dist/gameplay.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Gameplay = void 0;
-class Gameplay {
-    constructor(Player) { }
-}
-exports.Gameplay = Gameplay;
