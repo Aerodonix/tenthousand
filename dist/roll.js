92ebf4b20fd44759df1e21d78e14f0bd897081ec
moep <moep@fedora.fritz.box>
2024-09-12 08:34:26 +0200

    Revert "init"
    This reverts commit 610682c2581a2740d9c77eae0d4e40a36af196cc.

--- dist/roll.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.roll = roll;
-function roll() {
-    return Math.floor(Math.random() * 6) + 1;
-}
--- test/roll.test.ts
+++
@@ -1,7 +0,0 @@
-import { roll } from '../src/roll'
-
-test('Roll should return a number between 1 and 6', () => {
-    const rolledValue: number = roll();
-    expect(rolledValue).toBeGreaterThanOrEqual(1); // Check if >= 1
-    expect(rolledValue).toBeLessThanOrEqual(6);    // Check if <= 6
-});
